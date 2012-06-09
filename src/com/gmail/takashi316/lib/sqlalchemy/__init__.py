47f5e29b5aaf9e767c1bf9eac8c671961bbbec64
Takashi SASAKI <takashi316@gmail.com>
2012-06-09 17:05:07 +0900

    takashi316.lib.sqlalchemy.getSession(subdirectory, file_stem)

--- src/com/gmail/takashi316/lib/sqlalchemy/__init__.py
+++ src/com/gmail/takashi316/lib/sqlalchemy/__init__.py
@@ -2,10 +2,32 @@ from __future__ import unicode_literals, print_function
 from com.gmail.takashi316.lib.sqlite import getSqliteUrl
 
 from sqlalchemy import create_engine as _create_engine
-engine = _create_engine(getSqliteUrl(), echo=False)
+from sqlalchemy.exc import SQLAlchemyError
+_engine = None
+def getEngine(subdirectory, file_stem):
+    if _engine is not None:
+        return _engine
+    _engine = _create_engine(getSqliteUrl(subdirectory, file_stem), echo=False)
+    return _engine
+    
 from sqlalchemy.orm.session import sessionmaker as _sessionmaker
-Session = _sessionmaker(bind=engine, autocommit=False)
-PersistentSession = _sessionmaker(bind=engine, autocommit=True)
+
+_SessionClass = None
+_subdirectory = None
+_file_stem = None
+def _getSessionClass(subdirectory, file_stem):
+    if _SessionClass is not None:
+        if _subdirectory != subdirectory and _file_stem != file_stem:
+            raise SQLAlchemyError("subdirecory and file_stem is not identical that given for the first time.")
+        return _SessionClass
+    _subdirectory = subdirectory
+    _file_stem = file_stem
+    _SessionClass = _sessionmaker(bind=getEngine(subdirectory, file_stem), autocommit=False)
+    return _SessionClass 
+
+def getSession(subdirectory, file_stem):
+    _getSessionClass(subdirectory, file_stem)
+    return _SessionClass()
 
 from unittest import TestCase, main
 if __name__ == "__main__":
--- src/com/gmail/takashi316/lib/sqlite/__init__.py
+++ src/com/gmail/takashi316/lib/sqlite/__init__.py
@@ -2,12 +2,16 @@ from __future__ import unicode_literals, print_function
 from com.gmail.takashi316.lib.file import getUserDirectory as _getUserDirectory
 from com.gmail.takashi316.lib.debug import *
 
-def getSqliteUrl():
+def getSqliteUrl(subdirectory, file_stem):
     import os.path
-    sqlite_path = os.path.join(_getUserDirectory(), "obomb.db")
+    sqlite_path = os.path.join(_getUserDirectory(subdirectory), file_stem + ".sqlite")
     info(sqlite_path) 
     sqlite_path_slash = sqlite_path.replace(os.path.sep, '/')
     info(sqlite_path_slash)
     sqlite_url = "sqlite:///" + sqlite_path_slash
     info(sqlite_url)
     return sqlite_url
+
+from unittest import TestCase, main
+if __name__ == "__main__":
+    main()
