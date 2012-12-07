155b975a68fda58c90efec3b6028c96d64121b50
Takashi SASAKI <takashi316@gmail.com>
2012-12-07 23:30:04 +0900

    unit test

--- src/com/gmail/takashi316/lib/file/__init__.py
+++ src/com/gmail/takashi316/lib/file/__init__.py
@@ -13,15 +13,29 @@ def getHomeDirectory():
     home_directory = os.path.expanduser("~") 
     if home_directory is None:
         raise IOError("can't get home directory")
+    return home_directory
 
 from unittest import TestCase, main
 
 class Test(TestCase):
     def setUp(self):
         TestCase.setUp(self)
+        file = open("./hello.py", "a+")
+        file.write("\"\"\"this is a hello module\"\"\"")
+        file.close()
     
     def tearDown(self):
         TestCase.tearDown(self)
+        os.remove("./hello.py")
+        
+    def testGetModuleName(self):
+        from com.gmail.takashi316.lib.file import hello
+        x = getModuleName(hello)
+        self.assertIsInstance(x, str)
+    
+    def testGetHomeDirectory(self):
+        x = getHomeDirectory()
+        self.assertIsInstance(x, unicode)
 
 if __name__ == "__main__":
     main()
